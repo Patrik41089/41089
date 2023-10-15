b515a37ee658b09ed7be304a678c12384e9e0312
Patrik41089 <147606390+Patrik41089@users.noreply.github.com>
2023-10-15 09:05:41 +0200

    Update Krystal Kingdom.py

--- Krystal Kingdom.py
+++ Krystal Kingdom.py
@@ -1,2 +1,18 @@
-cislo = 1
-print(cislo)
+
+def introScene():
+  directions = ["left","right","forward"]
+  print("*You are at a crossroad in the middle of a forest, you can go by 4 paths. To the left is a mysterious figure, to the right is suspiciously empty space and in front of you is a house.*")
+  userInput = ""
+  while userInput not in directions:
+    print("Options: forward/left/right/backward")
+    userInput = input()
+    if userInput == "left":
+      showMysteriousFigure()
+    elif userInput == "right":
+      showEmptySpace()
+    elif userInput == "forward":
+      House1()
+    elif userInput == "backward":
+      print("*Some unknown force prevents you from going back.*")
+    else: 
+      print("Please enter a valid option.")
